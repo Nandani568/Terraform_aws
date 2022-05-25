88e0f6904128a765d6ade3fdb664e8863d0f6019
Nandani Sah <68953498+Nandani568@users.noreply.github.com>
2022-05-25 16:00:32 +0530

    Add files via upload

---
+++ script in ec2/vars.tf
@@ -0,0 +1,31 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0c4f7023847b90238"
+    us-west-2 = "ami-0f9fc25dd2506cf6d"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
+
